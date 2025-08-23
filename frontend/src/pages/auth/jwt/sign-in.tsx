23482b5e0b02447c2b8d399ee0be91639d9d929b
rishimalhan <rmalhan0112@gmail.com>
2025-08-22 20:09:15 -0400

    Revert "..."
    This reverts commit d9bf7467d5f62702e62b955c4acd20d9e19c8eda.

--- frontend/src/pages/auth/jwt/sign-in.tsx
+++ frontend/src/pages/auth/jwt/sign-in.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <>
       <Helmet>
-        <title> Orison AI </title>
+        <title> {metadata.title} </title>
       </Helmet>
       <AuthenticationView />
     </>
